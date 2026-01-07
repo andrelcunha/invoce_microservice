8b158abfd9addf56a846c8eb9ab68c941e8c205c
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2026-01-07 17:07:39 -0300

    ♻️ refactor: organize dependency injection by grouping repositories, services, and command handlers

--- src/InvoiceMicroservice.Api/Extensions/DependencyInjection.cs
+++ src/InvoiceMicroservice.Api/Extensions/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using InvoiceMicroservice.Application.Commands.EmitInvoice;
 using InvoiceMicroservice.Domain.Interfaces;
 using InvoiceMicroservice.Infrastructure.Repositories;
 using InvoiceMicroservice.Infrastructure.Xml;
@@ -8,10 +9,18 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
     {
+        // Repositories
         services.AddScoped<IInvoiceRepository, InvoiceRepository>();
         services.AddScoped<IServiceTypeTaxMappingRepository, ServiceTypeTaxMappingRepository>();
         services.AddScoped<IMunicipalityRepository, MunicipalityRepository>();
+
+        // Services
         services.AddScoped<IIpmXmlBuilder, IpmXmlBuilder>();
+
+        // Command handlers
+        services.AddScoped<EmitInvoiceCommandHandler>();
+
+
         return services;
     }
 
