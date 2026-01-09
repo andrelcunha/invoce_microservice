b3e274966bef980efe4a1b53f72caaf0b4082d5f
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2026-01-09 10:00:45 -0300

    ♻️ refactor: add IssRate column to Invoices table and update model snapshot

---
+++ src/InvoiceMicroservice.Infrastructure/Migrations/20260109123820_AddIssRateToInvoice.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceMicroservice.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIssRateToInvoice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "IssRate",
+                table: "Invoices",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IssRate",
+                table: "Invoices");
+        }
+    }
+}
