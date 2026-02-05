a564a9f77d4d81ee730ddc31b0e40f1102820485
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2026-02-05 08:58:20 -0300

    ✨ feat: add IssuerEntity class to represent issuer details in the domain model

---
+++ src/InvoiceMicroservice.Domain/Entities/IssuerEntity.cs
@@ -0,0 +1,23 @@
+using InvoiceMicroservice.Domain.Enums;
+
+namespace InvoiceMicroservice.Domain.Entities;
+
+public class IssuerEntity
+{
+    Guid Id { get; set; }
+    public string Cnpj { get; set; } = null!;
+    public string MunicipalInscription { get; set; } = null!;
+    public string TradeName { get; set; } = null!;
+    public string LegalName { get; set; } = null!;
+    public string Cnae { get; set; } = null!;
+    public string AddressJson { get; set; } = null!;
+    public RegimeTributario RegimeTributario { get; set; }
+    public SubRegimeTributario SubRegimeTributario { get; set;}
+
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public ICollection<PortalCredentials> PortalCredentials { get; set; } = [];
+}
+
